cf15c5d103ca7e0c9f51a667d5f81f7061d4995a
Muhamad Ivan Fadillah <ivanfadillah153@gmail.com>
2024-05-25 13:36:27 +0700

    Add colors in colors constant

--- lib/utils/constants/colors_constant.dart
+++ lib/utils/constants/colors_constant.dart
@@ -1,4 +1,12 @@
+import 'dart:ui';
+
 class ColorsConstant {
-  // example:
-  // static Color white = const Color(0xFFFFFFFF);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color neutral100 = const Color(0xFFF5F5F5);
+  static Color neutral200 = const Color(0xFFEDEDED);
+  static Color neutral400 = const Color(0xFFD4D4D4);
+  static Color neutral500 = const Color(0xFFA3A3A3);
+  static Color primary500 = const Color(0xFF1C6758);
+  static Color danger500 = const Color(0xFFD32D28);
 }
