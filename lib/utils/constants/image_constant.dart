2572664b9f9ca440763842a5bf36a601958666f6
BillyOde03 <naubilode@gmail.com>
2024-05-25 14:35:50 +0800

    Menambahkan gambar const untuk introduction

---
+++ lib/utils/constants/image_constant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ImageConstant {
+  static Image logoSplash = Image.asset(
+    "assets/introduction/greeve_logo.png",
+  );
+  static Image screenOne = Image.asset(
+    "assets/introduction/greeve_logo.png",
+  );
+  static Image screenTwo = Image.asset(
+    "assets/introduction/greeve_logo.png",
+  );
+  static Image screenThree = Image.asset(
+    "assets/introduction/greeve_logo.png",
+  );
+}
