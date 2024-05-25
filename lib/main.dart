bde8de5c6a026ca6e335c524588aa800397e2927
Muhamad Ivan Fadillah <ivanfadillah153@gmail.com>
2024-05-25 21:41:13 +0700

    Clear issue naubil

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greeve/utils/constants/routes_constant.dart';
 import 'package:greeve/view/auth_flow/screens/login_screen.dart';
-import 'package:greeve/view/auth_flow/introduction/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +21,9 @@ class MyApp extends StatelessWidget {
       ),
       getPages: [
         GetPage(
-            name: RoutesConstant.splashApp, page: () => const LoginScreen()),
+          name: RoutesConstant.splashApp,
+          page: () => const LoginScreen(),
+        ),
       ],
     );
   }
