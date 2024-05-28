e0b60156c672660b20da000ef5f3cece2f8d464c
BillyOde03 <naubilode@gmail.com>
2024-05-28 09:17:22 +0800

    repair file

--- lib/utils/constants/routes_constant.dart
+++ lib/utils/constants/routes_constant.dart
@@ -1,5 +1,5 @@
 class RoutesConstant {
-  static const String splashApp = '/';
+  static const String splashApp = '/splash';
   static const String login = '/login';
   static const String forgotPassword = '/forgot_password';
   static const String otp = '/otp';
