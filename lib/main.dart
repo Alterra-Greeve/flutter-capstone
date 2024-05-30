11f9358448680d38ed97bae5181c69c80b9f603d
Muhamad Ivan Fadillah <ivanfadillah153@gmail.com>
2024-05-30 14:52:52 +0700

    Delete app pages on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:greeve/routes/app_pages.dart';
 import 'package:greeve/utils/constants/colors_constant.dart';
 import 'package:greeve/routes/app_routes.dart';
-import 'package:greeve/view/forgot_password/screens/confirmation_screen.dart';
-import 'package:greeve/view/forgot_password/screens/forgot_password_screen.dart';
-import 'package:greeve/view/forgot_password/screens/new_pass_screen.dart';
-import 'package:greeve/view/forgot_password/screens/otp_screen.dart';
-import 'package:greeve/view/login/screens/loading_screen.dart';
-import 'package:greeve/view/login/screens/login_screen.dart';
-import 'package:greeve/view/register/register_success_screen.dart';
-import 'package:greeve/view/register/screens/register_screen.dart';
-import 'package:greeve/view/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,44 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       initialRoute: AppRoutes.splashApp,
-      getPages: [
-        GetPage(
-          name: AppRoutes.splashApp,
-          page: () => const SplashScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.forgotPassword,
-          page: () => const ForgotPassScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.login,
-          page: () => const LoginScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.otp,
-          page: () => const OtpScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.confirmPassword,
-          page: () => const ConfirmPassScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.register,
-          page: () => const RegisterScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.registerSuccess,
-          page: () => const RegisterSuccessScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.newPassword,
-          page: () => const NewPasswordScreen(),
-        ),
-        GetPage(
-          name: AppRoutes.loading,
-          page: () => const LoadingScreen(),
-        ),
-      ],
+      getPages: AppPages.routes,
     );
   }
 }
