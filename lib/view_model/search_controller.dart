36e9b32bd6f4a81cbd0fb270b1a93584a9528c84
ariff-m <21081010199@student.upnjatim.ac.id>
2024-06-09 06:05:24 +0700

    add search feature unfinished

--- lib/view_model/search_controller.dart
+++ lib/view_model/search_controller.dart
@@ -1,5 +1,63 @@
 import 'package:get/get.dart';
+import 'package:greeve/utils/constants/images_constant.dart';
 
-class SearchController extends GetxController {
-  
+class SearchProductController extends GetxController {
+  var isTextFieldFocused = false.obs;
+
+  void setFocus(bool isFocused) {
+    isTextFieldFocused.value = isFocused;
+  }
+
+  var products = [
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+    {
+      'name': 'Tumbler',
+      'price': 'Rp 148.500',
+      'image': ImagesConstant.bottleGrid
+    },
+  ].obs;
+
+  var searchQuery = ''.obs;
+
+  void updateSearchQuery(String query) {
+    searchQuery.value = query;
+  }
+
+  void navigateToCart() {
+    Get.toNamed('/cart');
+  }
 }
