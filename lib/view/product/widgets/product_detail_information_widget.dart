a5d1b56a46937fa2bb9e843e4c997def96130c9f
Muhamad Ivan Fadillah <ivanfadillah153@gmail.com>
2024-06-11 13:29:32 +0700

    Fix overflow on detail product

--- lib/view/product/widgets/product_detail_information_widget.dart
+++ lib/view/product/widgets/product_detail_information_widget.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:greeve/utils/constants/text_styles_constant.dart';
 import 'package:greeve/view/product/widgets/shimmer/paragraph_shimmer_widget.dart';
 import 'package:greeve/view_model/detail_product_controller.dart';
+import 'package:intl/intl.dart';
 
 class ProductDetailInformationWidget extends StatelessWidget {
   final DetailProductController controller;
@@ -22,9 +23,11 @@ class ProductDetailInformationWidget extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        controller.productData.value?.name ?? "-",
-                        style: TextStylesConstant.nunitoSemiboldTitle,
+                      Flexible(
+                        child: Text(
+                          controller.productData.value?.name ?? "-",
+                          style: TextStylesConstant.nunitoSemiboldTitle,
+                        ),
                       ),
                       Row(
                         children: [
@@ -38,22 +41,13 @@ class ProductDetailInformationWidget extends StatelessWidget {
                           // ),
                           // const SizedBox(width: 4),
                           Text(
-                            "Rp ${controller.productData.value?.price.toString() ?? '-'}",
+                            "Rp ${controller.productData.value?.price != null ? NumberFormat.currency(locale: 'id_ID', symbol: '', decimalDigits: 0).format(controller.productData.value!.price) : '-'}",
                             style: TextStylesConstant.nunitoSubtitle3,
                           ),
                         ],
                       ),
                     ],
                   ),
-                  // const SizedBox(height: 16),
-                  // Text(
-                  //   'Bahan: Stainless steel 304\n'
-                  //   'Kapasitas: 500 ml (17 oz)\n'
-                  //   'Dimensi: 23 cm x 7 cm (tinggi x diameter)\n'
-                  //   'Berat: 300 gram\n'
-                  //   'Warna: Silver, Biru, Hijau, Pink',
-                  //   style: TextStylesConstant.nunitoCaption,
-                  // ),
                   const SizedBox(height: 16),
                   Text(
                     controller.productData.value?.description ?? "-",
