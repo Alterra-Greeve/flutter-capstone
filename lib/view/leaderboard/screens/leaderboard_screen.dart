27105f7a6ee2c5fe29f61f531a626a5cc5700362
BillyOde03 <naubilode@gmail.com>
2024-06-19 23:48:57 +0800

    Repair Folder Structuure

--- lib/view/leaderboard/screens/leaderboard_screen.dart
+++ lib/view/leaderboard/screens/leaderboard_screen.dart
@@ -2,19 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:greeve/models/api_responses/leaderboard_response_model.dart';
-import 'package:greeve/utils/constants/colors_constant.dart';
-import 'package:greeve/utils/constants/icons_constant.dart';
 import 'package:greeve/utils/constants/images_constant.dart';
-import 'package:greeve/utils/constants/text_styles_constant.dart';
-import 'package:greeve/view/leaderboard/screens/boardsheet_stats.dart';
-import 'package:greeve/view/leaderboard/screens/bottomsheet_board_stats.dart';
-import 'package:greeve/view/leaderboard/screens/bottomsheet_board_stats_two.dart';
-import 'package:greeve/view/leaderboard/screens/container_info.dart';
-import 'package:greeve/view/leaderboard/screens/empty_stats.dart';
-import 'package:greeve/view/leaderboard/screens/leaderboard_appbar.dart';
-import 'package:greeve/view/leaderboard/screens/placement.dart';
+import 'package:greeve/view/leaderboard/screens/widgets/bottomsheet_board_stats.dart';
+import 'package:greeve/view/leaderboard/widgets/container.dart';
+import 'package:greeve/view/leaderboard/widgets/leaderboard_app_bar.dart';
+import 'package:greeve/view/leaderboard/widgets/placement.dart';
 import 'package:greeve/view_model/leaderboard_controller.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class LeaderboardScreen extends StatelessWidget {
   const LeaderboardScreen({super.key});
@@ -22,26 +15,9 @@ class LeaderboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final LeaderboardController _leaderController = LeaderboardController();
-    Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       extendBodyBehindAppBar: true,
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: Text(
-      //     'Leaderboard',
-      //     style: TextStylesConstant.nunitoHeading4,
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //       onPressed: () {
-      //         _leaderController.showHelpBottomSheet(context);
-      //       },
-      //       icon: Image.asset(IconsConstant.help_),
-      //     ),
-      //   ],
-      //   backgroundColor: Colors.transparent,
-      // ),
       body: Stack(
         children: [
           // Gradient Background
@@ -93,7 +69,7 @@ class LeaderboardScreen extends StatelessWidget {
           // Placement Bar
           Placement(),
           // Sliding Up Panel
-          BoardsheetStats()
+          BottomsheetBoardStats()
         ],
       ),
     );
